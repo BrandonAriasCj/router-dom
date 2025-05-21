e9a1afad3906c320b67a4128dea721ef3e7771c3
Danna <danna.mendoza@tecsup.edu.pe>
2025-05-21 14:24:00 -0500

    addidng public images

--- src/pages/SeriePage.jsx
+++ src/pages/SeriePage.jsx
@@ -4,12 +4,12 @@ import SerieComponent from "../components/SerieComponent"
 
 function SeriePage(){
     const series = [
-        {cod:1, nom:"Friends", cat:"Comedy", img:"friends.png"},
-        {cod:2, nom:"Law & Order", cat:"Drama", img:"law-and-order.png"},
-        {cod:3, nom:"The Big Bang Theory", cat:"Comedy", img:"the-big-bang.png"},
-        {cod:4, nom:"Stranger Things", cat:"Horror", img:"stranger-things.png"},
-        {cod:5, nom:"Dr. House", cat:"Drama", img:"dr-house.png"},
-        {cod:6, nom:"The X-Files", cat:"Drama", img:"the-x-files.png"},
+        {cod:1, nom:"Friends", cat:"Comedy", img:"friends.jpg"},
+        {cod:2, nom:"Law & Order", cat:"Drama", img:"law-and-order.jpg"},
+        {cod:3, nom:"The Big Bang Theory", cat:"Comedy", img:"tbbt"},
+        {cod:4, nom:"Stranger Things", cat:"Horror", img:"st.jpg"},
+        {cod:5, nom:"Dr. House", cat:"Drama", img:"dr-house.jpg"},
+        {cod:6, nom:"The X-Files", cat:"Drama", img:"the-x-files.jpg"},
       ];
 
 
